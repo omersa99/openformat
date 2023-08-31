9316d4a856d0f2cb1461c682d1adc2685b1cae72
omersa99 <omars@mail.tau.ac.il>
2023-08-31 12:49:51 +0300

    default creations

--- server/src/business/business.service.ts
+++ server/src/business/business.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { BusinessServiceBase } from "./base/business.service.base";
+import { Prisma, Business } from "@prisma/client";
 
 @Injectable()
 export class BusinessService extends BusinessServiceBase {
   constructor(protected readonly prisma: PrismaService) {
     super(prisma);
+  }
+
+  async create<T extends Prisma.BusinessCreateArgs>(args: Prisma.SelectSubset<T, Prisma.BusinessCreateArgs>): Promise<Business> {
+    const newBusiness = await super.create(args);
+
+    await this.prisma.setting.create({
+      data: {
+        business: {
+          connect: { id: newBusiness.id },
+        },
+      },
+    });
+
+    return newBusiness;
   }
 }
--- server/src/clientsAndSupplier/clientsAndSupplier.service.ts
+++ server/src/clientsAndSupplier/clientsAndSupplier.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { ClientsAndSupplierServiceBase } from "./base/clientsAndSupplier.service.base";
+import { Prisma, ClientsAndSupplier, Account } from "@prisma/client";
 
 @Injectable()
 export class ClientsAndSupplierService extends ClientsAndSupplierServiceBase {
   constructor(protected readonly prisma: PrismaService) {
     super(prisma);
+  }
+
+  async create<T extends Prisma.ClientsAndSupplierCreateArgs>(args: Prisma.SelectSubset<T, Prisma.ClientsAndSupplierCreateArgs>): Promise<ClientsAndSupplier> {
+    const clientsAndSupplierObj = await super.create(args);
+
+    await this.prisma.account.create({
+      data: {
+        clientsAndSuppliersId: clientsAndSupplierObj.id,
+        accountName: clientsAndSupplierObj.id,
+      },
+    });
+
+    return clientsAndSupplierObj;
   }
 }
