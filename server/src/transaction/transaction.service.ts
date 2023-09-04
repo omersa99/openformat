ba118b6843a954fe946e0704ff94403c38812ea8
omersa99 <omars@mail.tau.ac.il>
2023-09-04 22:05:53 +0300

    changes

--- server/src/transaction/transaction.service.ts
+++ server/src/transaction/transaction.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { TransactionServiceBase } from "./base/transaction.service.base";
+import { Prisma, Transaction } from "@prisma/client";
 
 @Injectable()
 export class TransactionService extends TransactionServiceBase {
   constructor(protected readonly prisma: PrismaService) {
     super(prisma);
+  }
+  async create<T extends Prisma.TransactionCreateArgs>(args: Prisma.SelectSubset<T, Prisma.TransactionCreateArgs>): Promise<Transaction> {
+    const newTransaction = await super.create(args);
+
+    return newTransaction;
   }
 }
